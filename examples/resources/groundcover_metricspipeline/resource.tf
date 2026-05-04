b8195c5f74f3c6cffe7ea3363edc70c3e92fd09e
Ori Shussman <orishuss@gmail.com>
2026-05-04 12:41:18 +0000

    example

--- examples/resources/groundcover_metricspipeline/resource.tf
+++ examples/resources/groundcover_metricspipeline/resource.tf
@@ -1,6 +1,5 @@
 resource "groundcover_metricspipeline" "example" {
   rules = {
-    keep_regex = ["http_requests_total", "process_cpu_seconds_total"]
     drop_regex = ["go_.*"]
     add_label = {
       team = "platform"
